65ea4360ccdea893d133c70f448e19b07bb7489f
TGaa232323 <goudie20231204@163.com>
2025-05-13 14:34:41 +0800

    Add files via upload

---
+++ www.huangjinjiage.cn/static/js/default.js
@@ -0,0 +1,32 @@
+// JavaScript Document
+
+function mgold() 
+    { 
+	document.writeln('<script type="text/javascript" src="//static.ichehome.com/source/lg/f-iop/resource/yc/source/v.js"></script>');
+	document.writeln('<script type="text/javascript" src="//static.ichehome.com/site/fa_z/x/common/zjgz_s.js"></script>');
+	}
+
+function mxxl() 
+    { 
+       document.writeln('<script type="text/javascript" src="//static.ichehome.com/source/lg/f-iop/resource/yc/source/v.js"></script>');
+	}
+function pcgold() 
+    { 
+	  document.writeln('<script type="text/javascript" src="//static.ichehome.com/production/kfe/resource/e/l_u_exn.js"></script>');
+    }
+function ziying() 
+    { 
+	  document.writeln('<a href="https://s.click.taobao.com/WvbPofu" target="_blank"><img src="http://res.huangjinjiage.com.cn/img/20211111750-330.jpg"/></a>');
+    }
+function comment1() 
+    { 
+    document.writeln('<script type="text/javascript" src="//static.ichehome.com/common/faz/openjs/x-zj/gzs.js"></script>');
+	}
+	function comment2() 
+    { 
+    document.writeln('<script type="text/javascript" src="//static.ichehome.com/site/dy/openjs/q/common/vu/xex-h.js"></script>');
+	}
+function comment3() 
+    { 
+    document.writeln('<script type="text/javascript" src="//static.ichehome.com/common/f/as/resource/gcjw/p/source/i.js"></script>');
+	}
